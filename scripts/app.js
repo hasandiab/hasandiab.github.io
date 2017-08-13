362e3426daa2aeac69820e3755f265c820622a07
hasandiab <30975306+hasandiab@users.noreply.github.com>
2017-08-13 13:36:45 +0300

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+﻿
+var myimg = document.querySelector('img');
+
+myimg.onclick = function () {
+    var mysrc = myimg.getAttribute('src');
+    if (mysrc === "https://vignette2.wikia.nocookie.net/zelda/images/b/bf/Firefox.png/revision/latest?cb=20100123012209") {
+        myimg.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/e/e8/A_username_graphic_for_FireFox.png');
+    }
+    else {
+        myimg.setAttribute('src', 'https://vignette2.wikia.nocookie.net/zelda/images/b/bf/Firefox.png/revision/latest?cb=20100123012209');
+    }
+}
